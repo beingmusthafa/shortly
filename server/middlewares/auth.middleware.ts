02767238bc551fc4abe3c0efc7b9da45f892e350
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-03-30 02:17:17 +0530

    user auth middleware

---
+++ server/middlewares/auth.middleware.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from "express";
+import customError from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import DatabaseId from "../types/databaseId.type.js";
+import UsersRepository from "../repositories/users.repository.js";
+
+declare global {
+  namespace Express {
+    interface Request {
+      session: any;
+      user?: { _id: string | DatabaseId; name: string; email: string };
+    }
+  }
+}
+
+class AuthMiddleware {
+  private usersRepository: UsersRepository;
+
+  constructor(usersRepository: UsersRepository) {
+    this.usersRepository = usersRepository;
+  }
+
+  async userAuth(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      if (!token) {
+        throw customError("Unauthorized", 401);
+      }
+      const { id } = jwt.verify(token, process.env.JWT_SECRET) as {
+        id: string;
+      };
+      if (req.session.user) return next();
+      req.session.user = await this.usersRepository.findOne(
+        { _id: id },
+        { password: 0 }
+      );
+      next();
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+}

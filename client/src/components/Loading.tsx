b56f88780fdc6885e052feca759b43df56015cd5
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-03-31 01:22:27 +0530

    loading gif

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -2,10 +2,7 @@ import loading from "../assets/shortly-loading.gif";
 
 const Loading = () => {
   return (
-    <div
-      style={{ backgroundColor: "rgba(255,255,255,0.3)" }}
-      className="w-full h-full _screen-center z-30"
-    >
+    <div className="min-w-full min-h-full _screen-center top-[30vh] z-30">
       <img src={loading} className="h-20 w-20" alt="" />
     </div>
   );

0f795f3dfc28a46d5cde0e117d7a63af227e7c02
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-03-30 23:20:43 +0530

    ...

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import { persistor, store } from "./state/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );

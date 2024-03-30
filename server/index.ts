64987aa46a214605b636ca95b7a08e879f1a093c
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-03-31 01:26:19 +0530

    index.ts basic setup completed

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,56 @@
 import express, { Request, Response, NextFunction } from "express";
 import connectToDb from "./config/database.js";
+import dotenv from "dotenv";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import userRouter from "./routes/user.router.js";
+import authRouter from "./routes/auth.router.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import cors from "cors";
+import LinksRepository from "./repositories/links.repository.js";
+import customError from "./utils/error.js";
 
 const app = express();
 connectToDb();
+dotenv.config();
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_BASE_URL,
+  })
+);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongoUrl: process.env.MONGO_URL }),
+  })
+);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRouter);
+app.use("/api/", userRouter);
+app.get(
+  "/:shortLink",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const linksRepository = new LinksRepository();
+      const { shortLink } = req.params;
+      const { originalLink } = (
+        await linksRepository.findOne({ shortLink })
+      ).toObject();
+      res.redirect(originalLink);
+    } catch (error) {
+      console.log(error);
+      next(customError(error.message, 500));
+    }
+  }
+);
+app.use(errorMiddleware);
+app.use("*", (req: Request, res: Response) => {
+  res.status(404).json({ message: "Page not found" });
+});
 
 app.listen(3000, () => {
   console.log("Server started");

67573adab55970ae4d46e18767860e6fbefeb89a
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-03-31 16:32:57 +0530

    optional chaining in auth middleware

--- server/middlewares/auth.middleware.ts
+++ server/middlewares/auth.middleware.ts
@@ -23,7 +23,7 @@ class AuthMiddleware {
 
   async userAuth(req: Request, res: Response, next: NextFunction) {
     try {
-      const token = req.headers.authorization.split(" ")[1];
+      const token = req.headers?.authorization?.split(" ")[1];
       if (!token) {
         throw customError("Unauthorized", 401);
       }

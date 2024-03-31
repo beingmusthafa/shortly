1a32a1f036c05554d13a3200c4279d6f0d803592
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-03-31 13:57:27 +0530

    client side 404

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <p className="text-2xl font-bold text-center mt-[40vh] text-orange-500">
+        404 | Page not found
+      </p>
+      <p className="text-xl text-center mt-4 text-slate-400">
+        Please check the URL
+      </p>
+    </>
+  );
+};
+
+export default NotFound;
